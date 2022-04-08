f98a8f37afafe96673e7b8c059b3a345d56d5dc6
ubuntu_x64_20_04_2022 <davidsamcun@gmail.com>
2022-04-08 14:23:44 -0400

    dependencies

---
+++ src/main/java/daos/getConnection.java
@@ -0,0 +1,12 @@
+package daos;
+import java.sql.*;
+
+public class getConnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc";
+    public static final String User = "master";
+    public static final String Pass = "admin";
+
+//    public static Connection getConnection(){
+//    }
+
+}
---
+++ src/main/java/models/Laptop.java
@@ -0,0 +1,72 @@
+package models;
+
+public class Laptop {
+
+    private Integer id;
+    private String make;
+    private String model;
+    private Integer year;
+    private String color;
+    private Integer cost;
+
+    public Laptop(Integer id){
+        this.id = id;
+    }
+
+    public Laptop(Integer id, String make, String model, Integer year, String color, Integer cost) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.cost = cost;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
